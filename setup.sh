26cbfa88fa0552fbe970d90bc3fc58d4596cde2d
gib <gib@gibbyb.com>
2023-06-15 14:23:07 -0500

    Remove flatpak add command as it crashed script and is unnecessary

--- setup.sh
+++ setup.sh
@@ -34,7 +34,6 @@ echo
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm 
 sudo dnf groupupdate core -y
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #################### INSTALL PACKAGES #########################
 
