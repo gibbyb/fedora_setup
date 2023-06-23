539e377891463aee91e7c8f2f4a68cfc58ce4613
gibbyb <gib@gibbyb.com>
2023-06-23 04:32:53 -0500

    Ugh

--- setup.sh
+++ setup.sh
@@ -93,25 +93,6 @@ sudo dnf install -y neovim xclip emacs git curl wget python3 python3-pip nodejs 
 echo 
 echo "Packages installed!"
 echo 
-
-#################### INSTALL FLATPAK PACKAGES #########################
-
-echo
-echo "Installing Flatpak packages..."
-echo
-flatpak install flathub -y sh.cider.Cider net.rpcs3.RPCS3 \
-    com.discordapp.Discord com.obsproject.Studio com.mojang.Minecraft \
-    org.libreoffice.LibreOffice org.yuzu_emu.yuzu net.davidotek.pupgui2 \
-    com.mattjakeman.ExtensionManager org.gnome.gThumb org.gnome.Geary \
-    org.gimp.GIMP org.kde.kdenlive com.slack.Slack com.github.xournalpp.xournalpp \
-    de.haeckerfelix.Fragments com.prusa3d.PrusaSlicer \
-    org.freecadweb.FreeCAD org.videolan.VLC com.bitwarden.desktop \
-    com.github.PintaProject.Pinta com.heroicgameslauncher.hgl
-    
-# com.microsoft.Edge com.visualstudio.code giving Microsoft Edge RPM a chance even though it is 
-# literally not as good as the flatpak, but it works with Webstorm, which is 
-# a whole other problem on why I am even using Webstorm in the first place
-
 #################### REMOVE PACKAGES WE DO NOT WANT #########################
 
 echo 
@@ -247,13 +228,6 @@ doom sync
 
 sudo cp ./nano/nanorc /etc/nanorc
 
-######################### SET UP GIT ################################
-git config --global user.name "gibbyb"
-git config --global user.email "gib@gibbyb.com"
-git config --global core.editor "nvim"
-git config --global init.defaultBranch "main"
-gh auth login
-
 ################## INSTALL NVIDIA DRIVERS #########################
 echo 
 echo "Installing Nvidia Drivers"
@@ -264,7 +238,7 @@ echo "Remove the duplicate lines \"rd.driver.blacklist=nouveau, \
     modprobe.blacklist=nouveau, and nvidia-drm.modeset=1\""
     kitty -1 -e bash -c "sudo nvim /etc/default/grub"
 read -p "Once complete, close neovim and press enter to continue."
-grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg 
+sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg 
 echo
 echo "############## WARNING ################"
 echo "Wait 5 minutes before rebooting!"
@@ -275,13 +249,6 @@ echo
 echo "Enabling Nvidia system services"
 sudo systemctl enable nvidia-hibernate.service nvidia-suspend.service \
     nvidia-resume.service
-echo
-echo "Enrolling Key to MOK"
-echo
-sudo mokutil --import /etc/pki/akmods/certs/public_key.der
-echo
-echo "Enroll the key once you reboot."
-echo 
 
 ################## SET UP WIREGUARD #########################
 
