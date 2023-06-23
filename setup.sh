6c23833679ad400ac3eda6c7ec937821b041bb37
gibbyb <gib@gibbyb.com>
2023-06-23 06:52:18 -0500

    So sick of this crap

--- setup.sh
+++ setup.sh
@@ -74,7 +74,6 @@ echo
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm 
 sudo dnf groupupdate core -y
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
 printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
@@ -236,7 +235,8 @@ echo
 sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
 echo "Remove the duplicate lines \"rd.driver.blacklist=nouveau, \
     modprobe.blacklist=nouveau, and nvidia-drm.modeset=1\""
-    kitty -1 -e bash -c "sudo nvim /etc/default/grub"
+kitty -1 -e bash -c "sudo nvim /etc/default/grub"
+kitty -1 -e bash -c "sudo nvim /etc/gdm/custom.conf"
 read -p "Once complete, close neovim and press enter to continue."
 sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg 
 echo
